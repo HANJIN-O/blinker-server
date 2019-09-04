494a827f4c92b34fd3843c6eeac29b8b82a03dcd
HAN-O <zeros0623@gmail.com>
2019-09-04 15:24:46 +0900

    add package morgan

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes.js");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/", router);
 app.listen(5000, () => {
